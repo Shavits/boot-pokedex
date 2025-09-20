4273a4d89860e5fb78416367002b66471db7bf00
shavits <s.shavit4@gmail.com>
2025-09-20 18:43:40 +0300

    added commands

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Println("Pokedex >")
-		scanner.Scan()
-		input := scanner.Text()
-		cleaned := cleanInput(input)
-		if len(cleaned) == 0 {
-			fmt.Println("Please enter command")
-			continue
-		}
-		fmt.Printf("Your command was: %s\n", cleaned[0])
-	}
-
-}
-
-func cleanInput(text string) []string {
-	trimmed := strings.TrimSpace(text)
-	lowered := strings.ToLower(trimmed)
-	words := strings.Fields(lowered)
-	return words
+	startRepl()
 }
